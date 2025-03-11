7594f4124ea889698e9a62ac603cda197980dec5
Thanuja K <119279936+Thanujak16@users.noreply.github.com>
2025-03-11 15:22:54 +0530

    Create Sumofseries.py

---
+++ Sumofseries.py
@@ -0,0 +1,13 @@
+print("Enter size of list")
+n = int(input())
+l = []
+num = 0
+print("Enter the elements")
+for i in range(0, n, 1):
+    num = int(input())
+    l.append(num)
+print(l)
+sum = 0
+for i in l:
+    sum = sum + i
+print(sum)
